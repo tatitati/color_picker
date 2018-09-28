08497f274b4f98f99e7713b5a7ea914e69fbec3b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-28 16:29:28 +0100

    added comments

--- color-pick/ColorPickerVC.swift
+++ color-pick/ColorPickerVC.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ColorPickerVC: UIViewController {
 
+    //
+    // This variable will be our ColorPresenterVC
+    //
     var delegate: ColorTransferDelegate? = nil
     
     
@@ -21,7 +24,15 @@ class ColorPickerVC: UIViewController {
     
     @IBAction func colorBtnWasPressed(sender: UIButton) {
         print(sender.titleLabel!.text!)
-        delegate?.colorDidChoose(color: sender.backgroundColor!,
-                withName: sender.titleLabel!.text!)
+        
+        //
+        // we call ColorPresenterVC::colorDidChoose()
+        // In this wy we can change paramters in the other view, we 
+        // the view in here!
+        //
+        delegate?.colorDidChoose(
+            color: sender.backgroundColor!,
+            withName: sender.titleLabel!.text!
+        )
     }
 }
--- color-pick/ColorPresenterVC.swift
+++ color-pick/ColorPresenterVC.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+//
+// ColorTransferDelegate is a protocol
+//
 class ColorPresenterVC: UIViewController, ColorTransferDelegate {
 
     @IBOutlet weak var colorNameLbl: UILabel!
@@ -17,15 +20,31 @@ class ColorPresenterVC: UIViewController, ColorTransferDelegate {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    //
+    // function specified in protocol
+    //
     func colorDidChoose(color: UIColor, withName colorName: String) {
         view.backgroundColor = color
         colorNameLbl.text = colorName
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //
+        // We the segue we can get the other view in here like any other
+        // reference/variable. That means that we can set variables in here
+        // In here we pass THIS view to the other, so the other will be able
+        // to modify this as well.
+        //
         if segue.identifier == "presentColorPickerVC" {
             guard let ColorPickerVC = segue.destination as?
-                ColorPickerVC else { return }
+                ColorPickerVC
+            else {
+                    return
+            }
+            
+            //
+            // This is the key!
+            //
             ColorPickerVC.delegate = self
         }
     }
